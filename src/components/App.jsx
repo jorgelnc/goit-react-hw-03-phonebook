f00c6cbc95b8a8325348086bd149c03474387bcc
jorgelnc <jorgenavacolina@gmail.com>
2023-10-05 10:03:01 -0500

    updated files task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,29 @@ import styled from 'styled-components';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+
+    if (parseContacts) {
+      this.setState({
+        contacts: parseContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const contacts = this.state.contacts;
+    const prevStateContacts = prevState.contacts;
+
+    if (contacts !== prevStateContacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   formSubmitHandler = data => {
     const checkName = this.state.contacts.find(el => el.name === data.name);
